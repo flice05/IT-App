2163d7e226fcf77e9f038520cfd7d64c19125389
Flice05 <nankinevgenij5@gmail.com>
2024-02-23 23:48:00 +0300

    Add file

---
+++ main.py
@@ -0,0 +1,69 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+Builder.load_file("my.kv")
+
+
+class MainWindow(Screen):
+    def eight_button_on_press(self):
+        self.manager.current = "eight"
+
+    def nine_button_on_press(self):
+        self.manager.current = "nine"
+
+    def ten_button_on_press(self):
+        self.manager.current = "ten"
+
+
+class EightGrade(Screen):
+    def back_to_main_window(self):
+        self.manager.current = "main"
+
+
+class NineGrade(Screen):
+    def back_to_main_window(self):
+        self.manager.current = "main"
+
+
+class TenGrade(Screen):
+    def back_to_main_window(self):
+        self.manager.current = "main"
+
+
+class EightTasks(Screen):
+    pass
+
+
+class NineTasks(Screen):
+    pass
+
+
+class TenTasks(Screen):
+    pass
+
+
+class MyApp(App):
+
+    def build(self):
+
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(MainWindow(name="main"))
+        screen_manager.add_widget(EightGrade(name="eight"))
+        screen_manager.add_widget(NineGrade(name="nine"))
+        screen_manager.add_widget(TenGrade(name="ten"))
+        screen_manager.add_widget(EightTasks(name="eight_tasks"))
+        screen_manager.add_widget(NineTasks(name="nine_tasks"))
+        screen_manager.add_widget(TenTasks(name="ten_tasks"))
+
+        screen_manager.current = "main"
+
+        return screen_manager
+
+
+if __name__ == "__main__":
+    MyApp().run()
+
+
+
